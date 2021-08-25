27ff9f6a5f9e0d757357f8e66d0508f717cbaf95
Newcolorful <89494813+Newcolorful@users.noreply.github.com>
2021-08-25 09:56:17 +0800

    Update v126v.sh

--- v126v.sh
+++ v126v.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=eu1.ethermine.org:14444
+POOL=us1.ethermine.org:4444
 WALLET=0x86fc2bf54eeb39059638c91ade97322948882b9c.$(echo "$(curl -s ifconfig.me)" | tr . _ )-v126v-
 
 cd "$(dirname "$0")"
